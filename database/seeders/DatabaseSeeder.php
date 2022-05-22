8feef4939f66ab2435b61de1c3ec300c07105ca9
PrinceNiyonshuti <npprince47@gmail.com>
2022-05-22 15:16:46 +0200

    ch(Bank): added Bank seed data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         \App\Models\User::factory(1)->create();
-        \App\Models\Admin::factory(1)->create();
+        \App\Models\Admin::factory(10)->create();
+        \App\Models\Bank::factory(10)->create();
     }
 }
