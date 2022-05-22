941fea0bdd7c783729ee3d07224b527a499da7a6
PrinceNiyonshuti <npprince47@gmail.com>
2022-05-22 15:16:02 +0200

    ch(Company): added company factory data

--- database/factories/CompanyFactory.php
+++ database/factories/CompanyFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,13 @@ class CompanyFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'companyName' => $this->faker->name(),
+            'companyAddress' => $this->faker->address(),
+            'details' => $this->faker->sentence(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
         ];
     }
 }
