5c6ce47c0471f90b5f79ef04f708337a57d085df
PrinceNiyonshuti <npprince47@gmail.com>
2022-05-22 15:09:39 +0200

    rf(User): refactored user factory data

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,11 +18,12 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => "Super admin",
-            'email' => "admin@gmail.com",
-            'email_verified_at' => now(),
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'imagePath' => $this->faker->imageUrl(),
+            'address' => $this->faker->address(),
+            'dateOfBirth' => $this->faker->date(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
         ];
     }
 
