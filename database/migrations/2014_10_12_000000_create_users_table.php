e02757a0bfa56e846aeb27ed30f066c96ce12a74
PrinceNiyonshuti <npprince47@gmail.com>
2022-05-22 15:08:58 +0200

    rf(User): refactored user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('reference')->unique();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('email');
+            $table->date('dateOfBirth')->nullable();
+            $table->string('imagePath')->nullable();
+            $table->string('address')->nullable();
+            $table->string('password')->nullable();
             $table->timestamps();
         });
     }
