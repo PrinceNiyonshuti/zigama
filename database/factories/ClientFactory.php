0c7a4438a9df70dfe1796e79cc8b0e7261a4ec69
PrinceNiyonshuti <npprince47@gmail.com>
2022-05-24 18:24:55 +0200

    ft(Client): added client factory data

--- database/factories/ClientFactory.php
+++ database/factories/ClientFactory.php
@@ -17,7 +17,12 @@ class ClientFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'names' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'imagePath' => $this->faker->imageUrl(),
+            'address' => $this->faker->address(),
+            'dateOfBirth' => $this->faker->date(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         ];
     }
 }
