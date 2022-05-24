fec199876bd92c7f4e97a239389675e3fbc22720
PrinceNiyonshuti <npprince47@gmail.com>
2022-05-24 18:34:56 +0200

    ft(Client): update client auth and model configurations

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -2,10 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
-class Client extends Model
+class Client extends Authenticatable
 {
-    use HasFactory;
+    use HasApiTokens, HasFactory, Notifiable;
+
 }
