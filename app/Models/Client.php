c1ac8c106f31199925494cca39e30f8e91a6204a
PrinceNiyonshuti <npprince47@gmail.com>
2022-05-25 22:53:49 +0200

    ft(Client): added casted data for client

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -21,4 +21,13 @@ class Client extends Authenticatable
         'password',
         'remember_token',
     ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
 }
