3ee80251c62ccd546de4d33547be440c54b36d72
PrinceNiyonshuti <npprince47@gmail.com>
2022-06-11 13:00:04 +0200

    ft(Seeder): added default data for the schema

--- database/seeders/BankAccountTypeSeeder.php
+++ database/seeders/BankAccountTypeSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\BankAccountType;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Schema;
@@ -17,6 +18,9 @@ class BankAccountTypeSeeder extends Seeder
     {
         //
         Schema::disableForeignKeyConstraints();
+        BankAccountType::truncate();
+        BankAccountType::create(['bank_id' => '1', 'typeName' => 'Personal']);
+        BankAccountType::create(['bank_id' => '2', 'typeName' => 'Bussiness']);
         Schema::enableForeignKeyConstraints();
     }
 }
